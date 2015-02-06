9f924bf354addfa679b71bd3c71498b34de4ec83
dmzx <info@dmzx-web.net>
2015-02-06 22:55:12 +0100

    lang file
    lang file

---
+++ language/hr/lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* Croatian translation by Ančica Sečan (http://ancica.sunceko.net)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+$lang = array();
+}
+
+$lang = array_merge($lang, array(
+'TOPIC_AUTHOR'=>'Autor/ica teme',
+)); 
